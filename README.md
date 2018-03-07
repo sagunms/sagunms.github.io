63a45693835e8a79fa2010cc42ec8d546cfffa08
Sagun Man Singh Shrestha <sagunms@msn.com>
2018-03-08 01:53:58 +1000

    Out of business for a while

--- README.md
+++
@@ -1,2 +0,0 @@
-# SaGuN's Blog
-Powered by Octopress
--- downloads/code/fib.js
+++
@@ -1,8 +0,0 @@
-// Start Fibonacci Generator function
-function fib(n) {
-	if (n <= 1) {
-		return 1;
-	}
-	return fib(n-2) + fib(n-1);
-}
-// End Fibonacci Generator function
